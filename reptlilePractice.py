9f1ca5e3e6e986a25c450f3221d98106a664a1f2
telils <telils@163.com>
2019-12-27 17:35:58 +0800

    找到新的教程，开始安装环境

--- reptlilePractice.py
+++ reptlilePractice.py
@@ -1,10 +1,27 @@
 #encoding=utf8
 import urllib.request  #py3.x 需要用urllib.request
 from bs4 import *
+from selenium import webdriver
 
-res = urllib.request.urlopen("http://www.douban.com/tag/%E5%B0%8F%E8%AF%B4/?focus=book")
-soup = BeautifulSoup.BeautifulSoup(res)
-book_div = soup.find(attrs={"id":"book"})
-book_a = book_div.findAll(attrs={"class":"title"})
-for book in book_a:
-    print(book.string)
+browser = webdriver.Chrome()
+
+res = urllib.request.urlopen("http://jwxt.qlu.edu.cn/jsxsd/jxzl/jxzl_query?Ves632DSdyV=NEW_XSD_WDZM")
+soup = BeautifulSoup(res,'lxml')
+# book_div = soup.find(attrs={"id":"book"})  #通过id=book获取div标签
+# book_a = book_div.findAll(attrs={"class":"title"})  #通过class="title"获取所有的book a标签
+# for book in book_a:
+#     print(book.string)
+
+html = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title" name="dromouse"><b>The Dormouse's story</b></p>
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1"><!-- Elsie --></a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+<p class="story">...</p>
+"""
+# soup = BeautifulSoup(html, 'lxml')
+print(soup.table)
