31b4ef9a34e027a87034daca63aa39bc9242e6c7
telils <telils@163.com>
2019-12-28 22:35:49 +0800

    熟悉response的方法

--- scrapyPractice.py
+++ scrapyPractice.py
@@ -1,4 +1,6 @@
 import urllib.request
 
 response = urllib.request.urlopen('http://www.python.org')
-print(type(response)) 
+print(response.status)
+print(response.getheaders())
+print(response.getheader('Server')) 
