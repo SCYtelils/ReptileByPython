42bbb64d688038016eef9d08e89253899dac4773
telils <telils@163.com>
2019-12-29 20:36:05 +0800

    结合timeout方法和try-except跳过超时抓取页面

--- scrapyPractice.py
+++ scrapyPractice.py
@@ -1,5 +1,10 @@
 import urllib.request
 import urllib.parse
+import socket
+import urllib.error
 
-response = urllib.request.urlopen('http://httpbin.org/get',timeout=0.1)
-print(response.read())
+try:
+    response = urllib.request.urlopen('http://httpbin.org/get',timeout=0.1)
+except urllib.error.URLError as e:
+    if isinstance(e.reason,socket.timeout):
+        print('TIME OUT')
