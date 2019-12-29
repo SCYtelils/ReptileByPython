858b26de222d82ad2e2ea30d7db2e96ca5a65d47
telils <telils@163.com>
2019-12-29 20:50:51 +0800

    模拟火狐浏览器爬取httpbin

--- scrapyPractice.py
+++ scrapyPractice.py
@@ -1,5 +1,11 @@
-import urllib.request
+from urllib import request,parse
 
-request = urllib.request.Request('https://python.org')
-response = urllib.request.urlopen(request)
+url = 'http://httpbin.org/post'
+headers = {'User-Agent':'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)',
+        'Host': 'httpbin.org'
+}
+dict = {'name':'Germey'}
+data = bytes(parse.urlencode(dict),encoding='utf8')
+req = request.Request(url=url,data=data,headers=headers,method = 'POST')
+response = request.urlopen(req)
 print(response.read().decode('utf-8'))
