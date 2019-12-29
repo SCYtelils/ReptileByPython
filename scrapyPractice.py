5641262a2b911ce49547774d18adaacd3cf4fb57
telils <telils@163.com>
2019-12-29 20:29:58 +0800

    熟悉urllib参数之一：timeout

--- scrapyPractice.py
+++ scrapyPractice.py
@@ -1,7 +1,5 @@
 import urllib.request
 import urllib.parse
 
-#bytes方法：将参数转化为字节流编码格式的内容，即bytes
-data = bytes(urllib.parse.urlencode({'word':'hello'}),encoding='utf8')  
-response = urllib.request.urlopen('http://httpbin.org/post',data=data)
+response = urllib.request.urlopen('http://httpbin.org/get',timeout=0.1)
 print(response.read())
