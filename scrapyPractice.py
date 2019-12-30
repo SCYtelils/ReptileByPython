f9acb37036451555f7490bdb35d96c9b75eeef1a
telils <telils@163.com>
2019-12-30 14:32:36 +0800

    将cookie保存到本地文档中

--- scrapyPractice.py
+++ scrapyPractice.py
@@ -1,8 +1,9 @@
 import http.cookiejar,urllib.request
 
-cookie = http.cookiejar.CookieJar()
+filename = 'cookies.txt'
+cookie = http.cookiejar.MozillaCookieJar(filename)
 handler = urllib.request.HTTPCookieProcessor(cookie)
 opener = urllib.request.build_opener(handler)
 response = opener.open('http://www.baidu.com')
-for item in cookie:
-    print(item.name+"="+item.value)
+cookie.save(ignore_discard=True,ignore_expires=True)
+
