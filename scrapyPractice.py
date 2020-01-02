9129013f6889c3674d0f71fd18754d2f8979b8b3
telils <telils@163.com>
2020-01-02 20:51:01 +0800

    parseresult的本质，元组

--- scrapyPractice.py
+++ scrapyPractice.py
@@ -1,4 +1,4 @@
 from urllib.parse import urlparse
 
 result = urlparse('http://www.baidu.com/index.html;user?id=5#comment')
-print(type(result),result)
+print(result.scheme,result[0],result.netloc,result[1],sep='\n')
