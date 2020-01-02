1b133a859f47d32ca4bd1cb2a5d28163cd8a9566
telils <telils@163.com>
2020-01-02 18:40:19 +0800

    LWP格式的cookie文件

--- scrapyPractice.py
+++ scrapyPractice.py
@@ -1,7 +1,7 @@
 import http.cookiejar,urllib.request
 
 filename = 'cookies.txt'
-cookie = http.cookiejar.MozillaCookieJar(filename)
+cookie = http.cookiejar.LWPCookieJar(filename)
 handler = urllib.request.HTTPCookieProcessor(cookie)
 opener = urllib.request.build_opener(handler)
 response = opener.open('http://www.baidu.com')
