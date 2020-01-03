408bb557146aa6000b3c3b927434d32b0550ea51
telils <telils@163.com>
2020-01-03 16:55:26 +0800

    尝试模拟浏览器获取知乎问题的题目，很可惜，什么都没有

--- scrapyPractice.py
+++ scrapyPractice.py
@@ -1,6 +1,9 @@
-import requests  
+import requests
+import re
 
-request = requests.get('http://httpbin.org/get')
-print(type(request.text))
-print(request.json())
-print(type(request.json()))
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
+}
+r = requests.get("https://www.zhihu.com/explore", headers=headers)
+pattern = re.compile('explore-feed.*?question_link.*?>(.*?)</a>', re.S)
+titles = re.findall(pattern, r.text)
+print(titles)
