72dee6ed3a3068e482c5fb8fdcaf2072a1f9f413
telils <telils@163.com>
2020-01-03 10:37:33 +0800

    逆解析unparse

--- scrapyPractice.py
+++ scrapyPractice.py
@@ -1,4 +1,4 @@
-from urllib.parse import urlparse
+from urllib.parse import urlunparse  
 
-result = urlparse('http://www.baidu.com/index.html;user?id=5#comment')
-print(result.scheme,result[0],result.netloc,result[1],sep='\n')
+data = ['http', 'www.baidu.com', 'index.html', 'user', 'a=6', 'comment']  
+print(urlunparse(data))
