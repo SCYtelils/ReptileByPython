08d4f1c7ce4cafcbdc07912dc198f91da658c010
telils <telils@163.com>
2020-01-03 16:35:32 +0800

    查看简书的robots.txt，并判断权限。很可惜，没法获取权限

--- scrapyPractice.py
+++ scrapyPractice.py
@@ -1,7 +1,6 @@
 from urllib.robotparser import RobotFileParser
-
+from urllib.request import urlopen
 rp = RobotFileParser()
-rp.set_url('http://www.jianshu.com/robot.txt')
-rp.read()
-print(rp.can_fetch('*','http://www.jianshu.com/p/b67554025d7d'))
-print(rp.can_fetch('*',"http://www.jianshu.com/serch?q=python&page=1&type=collections"))
+rp.parse(urlopen('http://www.jianshu.com/robots.txt').read().decode('utf-8').split('\n'))
+print(rp.can_fetch('*', 'http://www.jianshu.com/p/b67554025d7d'))
+print(rp.can_fetch('*', "http://www.jianshu.com/search?q=python&page=1&type=collections"))
