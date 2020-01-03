c855076eb5e03c11933eafc55eed70da833c892d
telils <telils@163.com>
2020-01-03 16:23:00 +0800

    使用urlencode拼接url传参

--- scrapyPractice.py
+++ scrapyPractice.py
@@ -1,10 +1,9 @@
-from urllib.parse import urljoin  
+from urllib.parse import urlencode
 
-print(urljoin('http://www.baidu.com', 'FAQ.html'))  
-print(urljoin('http://www.baidu.com', 'https://cuiqingcai.com/FAQ.html'))  
-print(urljoin('http://www.baidu.com/about.html', 'https://cuiqingcai.com/FAQ.html'))  
-print(urljoin('http://www.baidu.com/about.html', 'https://cuiqingcai.com/FAQ.html?question=2'))  
-print(urljoin('http://www.baidu.com?wd=abc', 'https://cuiqingcai.com/index.php'))  
-print(urljoin('http://www.baidu.com', '?category=2#comment'))  
-print(urljoin('www.baidu.com', '?category=2#comment'))  
-print(urljoin('www.baidu.com#comment', '?category=2'))
+params = {
+    'name' : 'germey',
+    'age' : 22
+}
+base_url = "http://www.baidu.com?"
+url = base_url + urlencode(params)
+print(url)
