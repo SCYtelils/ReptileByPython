7750ef2ab8e213176e3ed3ce63378f0cfa04b322
telils <telils@163.com>
2020-01-03 16:51:38 +0800

    使用json方法将json格式的字符串转化为字典

--- scrapyPractice.py
+++ scrapyPractice.py
@@ -1,8 +1,6 @@
 import requests  
 
-data = {
-    'name' : 'germey',
-    'age' : 22
-}
-request = requests.get('http://httpbin.org/get',params = data)
-print(request.text)
+request = requests.get('http://httpbin.org/get')
+print(type(request.text))
+print(request.json())
+print(type(request.json()))
